e0460eaf654111f8637b58b5e4ef37715850f42a
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-10-28 18:52:06 -0300

    new

---
+++ how-to-print-from-1-to-100-without-numbers/solution.js
@@ -0,0 +1,13 @@
+/**
+ *************************************************
+ *
+ *  Challenge: Print from 1 to 100 without using
+ *  numbers in your code.
+ * 
+ *************************************************
+ * @author Josélio de S. C. Júnior <joseliojrx25@gmail.com>
+ * @copyright Josélio de S. C. Júnior - 2021
+*/
+
+// Solution
+Array.from(Array(+`${'' ** ''}${+''}${+''}`), (_, idx) => console.log(++idx));
